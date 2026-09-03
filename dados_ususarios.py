3eda9a3aa7d3a96ddc25cdde19748df0b35a8283
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-03 14:57:30 -0300

    "O sabiá não sabia que o Sabio sabia que o sabiá não sabia assobiar "

--- dados_ususarios.py
+++ dados_ususarios.py
@@ -3,6 +3,9 @@ import sqlite3
 #conectando o banco de dados. Caso não exista, o banco é criado.
 conn = sqlite3.connect("biblioteca.db")
 
+
+'''
+#---USUÁRIOS---
 #apaga tabela ususarios
 conn.execute("DROP TABLE IF EXISTS usuarios")
 
@@ -14,7 +17,19 @@ conn.executemany("INSERT INTO usuarios(nome) VALUES(?)",
                  [("Jaiusom",), ("Simon",), ("Frodo",),])
 
 conn.executemany("INSERT INTO usuarios(nome) VALUES(?)", \
-                 [("The Rock Clark",),("Roberto Cruzez",),])
+                 [("The Rock Clark",),("Roberto Cruzez",),])'''
 
-#confrimando a criação e os inserts da tabela ususarios.
+#---EDITORAS---
+#apaga tabela editoras
+conn.execute("DROP TABLE IF EXISTS editoras")
+
+#cria a tabela editoras
+conn.execute("CREATE TABLE editoras (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL)")
+
+#inserindo os registros na tabela editoras.
+conn.executemany("INSERT INTO EDITORAS(nome) VALUES(?)",
+                 [("Darkside",), ("Intriseca",), ("Seguinte",),])
+
+
+#confrimando a criação e os inserts da tabela editoras.
 conn.commit()
