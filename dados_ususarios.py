0a9d138ee07aa357de7a5cc9b57950235fe93bc0
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-03 14:48:24 -0300

    "O sabiá não sabia que o Sabio sabia que o sabiá não sabia assobiar "

--- dados_ususarios.py
+++ dados_ususarios.py
@@ -11,8 +11,10 @@ conn.execute("CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome 
 
 #inserindo os registros na tabela ususarios.
 conn.executemany("INSERT INTO usuarios(nome) VALUES(?)",
-                 [("Jaisusom",), ("Simon",), ("Frodo",),])
+                 [("Jaiusom",), ("Simon",), ("Frodo",),])
+
+conn.executemany("INSERT INTO usuarios(nome) VALUES(?)", \
+                 [("The Rock Clark",),("Roberto Cruzez",),])
 
 #confrimando a criação e os inserts da tabela ususarios.
 conn.commit()
-
